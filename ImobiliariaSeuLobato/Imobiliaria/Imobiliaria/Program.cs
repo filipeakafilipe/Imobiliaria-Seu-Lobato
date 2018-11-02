61330999d8e536072161e317cccd7993e16495be
filipeakafilipe <filipeffb99@gmail.com>
2018-11-02 20:55:38 -0300

    Início
    Definindo as funções do programa

---
+++ ImobiliariaSeuLobato/Imobiliaria/Imobiliaria/Program.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Imobiliaria
+{
+    class Program
+    {
+
+        // Funções de Cadastro
+
+        public static void cadProp(string prop, string ende, int tel)
+        {
+
+            /* 
+             * cadProp
+             * Cadastra um novo propietário 
+             * Entrada:
+             * Saída:
+             */
+
+            FileStream arqProp = new FileStream("informacoes/proprietario.txt", FileMode.Open);
+            StreamWriter escProp = new StreamWriter(arqProp);
+
+
+        }
+
+        public static void cadCliente()
+        {
+
+            /* cadCliente
+             * Cadastra um novo cliente 
+             * Entrada:
+             * Saída:
+             */
+
+
+
+
+        }
+
+        public static void cadCorretor()
+        {
+
+            /* cadCorretor
+             * Cadastra um novo propietário 
+             * Entrada:
+             * Saída:
+             */
+
+
+
+        }
+
+        public static void cadImovel()
+        {
+
+            /* cadImovel
+             * Cadastra um novo imóvel 
+             * Entrada:
+             * Saída:
+             */
+
+
+
+        }
+
+        public static void cadLoc()
+        {
+
+            /* cadLoc
+             * Cadastra uma nova locação
+             * Entrada:
+             * Saída:
+             */
+
+
+
+        }
+
+        public static void cadVenda()
+        {
+
+            /* cadVenda
+             * Cadastra uma nova venda
+             * Entrada:
+             * Saída:
+             */
+
+
+
+        }
+
+        // Funções de Pesquisa
+
+        public static void pesquisa()
+        {
+
+            /* 
+             * pesquisa
+             * Pesquisa propietário 
+             * Entrada:
+             * Saída:
+             */
+
+            char opc;
+            string nome;
+
+            do
+            {
+                Console.WriteLine("Digite uma opção para pesquisa:" +
+                                "\n\nA. informações de clientes" +
+                                "\nB. informações de proprietários" +
+                                "\nC. informações de corretores" +
+                                "\nD. sair");
+                opc = char.Parse(Console.ReadLine().ToLower());
+
+                switch (opc)
+                {
+                    case 'a':
+
+                        Console.WriteLine("Nome:");
+                        nome = Console.ReadLine();
+                        pesCliente(nome);
+                        break;
+
+                    case 'b':
+
+                        Console.WriteLine("Nome:");
+                        nome = Console.ReadLine();
+                        pesProp(nome);
+                        break;
+
+                    case 'c':
+
+                        Console.WriteLine("Nome:");
+                        nome = Console.ReadLine();
+                        pesCorretor(nome);
+                        break;
+
+                    case 'd':
+
+                        Console.WriteLine("Saindo");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida, digite H para ver as opções");
+                        opc = char.Parse(Console.ReadLine().ToLower());
+                        if(opc == 'h')
+                        {
+                            Console.WriteLine("Opções para pesquisa:" +
+                                            "\n\nA. informações de clientes" +
+                                            "\nB. informações de proprietários" +
+                                            "\nC. informações de corretores" +
+                                            "\nD. sair");
+                        }
+                        break;
+
+                }
+
+            } while (opc != 'd');
+
+        }
+
+        public static void pesProp(string nome)
+        {
+
+            /* 
+             * pesProp
+             * Pesquisa propietário 
+             * Entrada:
+             * Saída:
+             */
+
+            FileStream arqProp = new FileStream("informacoes/proprietario.txt", FileMode.Open);
+            StreamReader leProp = new StreamReader(arqProp);
+
+
+        }
+
+        public static void pesCliente(string nome)
+        {
+
+            /* 
+             * pesCliente
+             * Pesquisa cliente 
+             * Entrada:
+             * Saída:
+             */
+
+            FileStream arqCliente = new FileStream("informacoes/cliente.txt", FileMode.Open);
+            StreamReader leCliente = new StreamReader(arqCliente);
+
+
+        }
+
+        public static void pesCorretor(string nome)
+        {
+
+            /* 
+             * pesCorretor
+             * Pesquisa corretor 
+             * Entrada:
+             * Saída:
+             */
+
+            FileStream arqCorretor = new FileStream("informacoes/corretor.txt", FileMode.Open);
+            StreamReader leCorretor = new StreamReader(arqCorretor);
+
+
+        }
+
+        // Relatório
+
+        public static void relCorretor()
+        {
+
+            /* relCorretor
+             * Mostra todas as vendas de imóveis de um corretor
+             * Entrada:
+             * Saída:
+             */
+
+
+
+        }
+
+        public static void relVenda()
+        {
+
+            /* relVenda
+             * Mostra todas as vendas de imóveis de uma determinada data
+             * Entrada:
+             * Saída:
+             */
+
+
+
+        }
+
+        public static void relLoc()
+        {
+
+            /* cadVenda
+             * Mostra todas as locações de imóveis de uma determinada data
+             * Entrada:
+             * Saída:
+             */
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
